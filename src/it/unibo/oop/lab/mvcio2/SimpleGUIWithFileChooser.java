53a9b8489d362673bb1f47060bb54a3cc7d7511b
Riccardo Fonti <riccardo.fonti@studio.unibo.it>
2021-11-26 10:16:45 +0100

    copy and modify mvcio, add textfield and button

--- src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
+++ src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
@@ -1,11 +1,48 @@
 package it.unibo.oop.lab.mvcio2;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import it.unibo.oop.lab.mvcio.Controller;
+
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUIWithFileChooser {
 
+    private final JFrame frame = new JFrame("Simple GUI with file chooser");
+
+    private SimpleGUIWithFileChooser() {
+        final Controller controller = new Controller();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JTextArea textArea = new JTextArea();
+        final JButton save = new JButton("Save");
+        final JPanel canvas = new JPanel(new BorderLayout());
+        canvas.add(textArea, BorderLayout.CENTER);
+        canvas.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(canvas);
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    controller.save(textArea.getText());
+                } catch (IOException e1) {
+                    JOptionPane.showMessageDialog(frame, e1, "Error, cannot save", JOptionPane.ERROR_MESSAGE);
+                    }
+            }
+        });
     /*
      * TODO: Starting from the application in mvcio:
      * 
@@ -14,8 +51,15 @@ public final class SimpleGUIWithFileChooser {
      * Suggestion: use a second JPanel with a second BorderLayout, put the panel
      * in the North of the main panel, put the text field in the center of the
      * new panel and put the button in the line_end of the new panel.
-     * 
-     * 2) The JTextField should be non modifiable. And, should display the
+     */ 
+    final JPanel panel = new JPanel(new BorderLayout());
+    final JTextField textField = new JTextField(controller.getPath());
+    textField.setEditable(false);
+    final JButton browse = new JButton("Browse...");
+    panel.add(textField, BorderLayout.NORTH);
+    panel.add(browse, BorderLayout.LINE_END);
+    canvas.add(panel, BorderLayout.NORTH);
+     /* 2) The JTextField should be non modifiable. And, should display the
      * current selected file.
      * 
      * 3) On press, the button should open a JFileChooser. The program should
@@ -31,5 +75,18 @@ public final class SimpleGUIWithFileChooser {
      * update the UI: in this example the UI knows when should be updated, so
      * try to keep things separated.
      */
-
+    final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+    final int sw = (int) screen.getWidth();
+    final int sh = (int) screen.getHeight();
+    frame.setSize(sw / 2, sh / 2);
+    frame.setLocationByPlatform(true);
+    }
+    /**
+     * 
+     * @param args not used
+     */
+    public static void main(final String... args) {
+        final SimpleGUIWithFileChooser gui = new SimpleGUIWithFileChooser();
+        gui.frame.setVisible(true);
+    }
 }
