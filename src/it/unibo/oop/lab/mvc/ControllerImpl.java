38a051267773b7b94dc39541e0b73d99939bc9a8
Riccardo Fonti <riccardo.fonti@studio.unibo.it>
2021-11-26 12:12:50 +0100

    implement interface, create 2 field

---
+++ src/it/unibo/oop/lab/mvc/ControllerImpl.java
@@ -0,0 +1,50 @@
+package it.unibo.oop.lab.mvc;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+public class ControllerImpl implements Controller {
+
+    private String nextString;
+    private final List<String> history = new LinkedList<>();
+
+    /**
+     * A method for setting the next string to print. Null values are not
+     * acceptable, and an exception should be produced.
+     */
+    @Override
+    public void setNextStringToPrint(final String text) {
+        this.nextString = Objects.requireNonNull(text);
+
+    }
+
+    /**
+     * A method for getting the next string to print.
+     */
+    @Override
+    public String getNextString() {
+        return this.nextString;
+    }
+
+    /**
+     * A method for getting the history of the printed strings (in form of a List
+     * of Strings).
+     */
+    @Override
+    public List<String> getPrintHistory() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * A method that prints the current string. If the current string is unset,
+     * an IllegalStateException should be thrown.
+     */
+    @Override
+    public String printCurrentString() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
